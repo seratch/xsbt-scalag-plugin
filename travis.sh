c1a5949b7b3bb3cc12f18bd705abaff34f958237
Kazuhiro Sera <seratch@gmail.com>
2012-09-21 12:06:55 +0900

    Added testing with 0.12.0

--- travis.sh
+++ travis.sh
@@ -2,4 +2,6 @@
 rm project/gpg.sbt
 wget https://raw.github.com/paulp/sbt-extras/master/sbt &&
 chmod u+x ./sbt &&
-./sbt -sbt-version 0.11.3 -mem 512 test scripted
+./sbt -sbt-version 0.11.3 -mem 512 test scripted &&
+./sbt -sbt-version 0.12.0 -mem 512 test scripted 
+
